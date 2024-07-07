2c6157c4a208541b11f7f2530c7ad0a9ba793760
Rafael <rafaelvpds.dev09@gmail.com>
2024-07-07 14:12:09 -0300

    create veiculo

--- src/main/java/com/api/veicular/Services/VeiculosService.java
+++ src/main/java/com/api/veicular/Services/VeiculosService.java
@@ -12,18 +12,7 @@ public class VeiculosService {
     VeiculosRepository repository;
 
     public Veiculo create(Veiculo veiculo) {
-        Veiculo newVeiculos = new Veiculo();
-        newVeiculos.setBrand(veiculo.getBrand());
-        newVeiculos.setChassisNumber(veiculo.getChassisNumber());
-        newVeiculos.setModel(veiculo.getModel());
-        newVeiculos.setPlaca(veiculo.getPlaca());
-        newVeiculos.setProprietaryDocument(veiculo.getProprietaryDocument());
-        newVeiculos.setProprietaryName(veiculo.getProprietaryName());
-        newVeiculos.setYearManifactory(veiculo.getYearManifactory());
-        newVeiculos.setYearModel(veiculo.getYearModel());
-        newVeiculos.setRenavam(veiculo.getRenavam());
-        repository.save(newVeiculos);
 
-        return newVeiculos;
+        return repository.save(veiculo);
     }
 }
